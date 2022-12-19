dca6c6e8b2b302337128e226e49e3dd3d55a13e2
Andres Soibelzon <a.soibelzon@gmail.com>
2022-12-19 23:00:55 +0000

    button add added

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,20 +1,34 @@
+window.onload = function () {
 
-    window.onload = function(){
-    
-    let button = document.getElementById("submit");
-    console.log(button);
+    let count = 3;
+
+let button = document.getElementById("toDoBtn");
+console.log(button);
+
+button.addEventListener('click', function () {
+
+    let ul = document.getElementById("toDolist");
+    console.log(ul);
     let input = document.getElementById("addToDo");
     console.log(input);
+    
+    count += 1;
 
-    button.addEventListener('click', function() {
-        let ul = document.getElementById("list"); 
-        
-        console.log(ul);
-        
-        let paragraph = document.createElement("input");
-        paragraph.innerHTML = input.value;
-        ul.appendchild(paragraph);
-        
-
-    })
-    }
+    let value = input.value;
+    console.log(value)
+    
+    
+    let li = document.createElement("li");
+    li.setAttribute("id", count)
+    
+    let span = document.createElement("span");
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fa fa-trash");
+    
+    span.appendChild(icon);
+    li.appendChild(span); 
+  
+    li.appendChild(document.createTextNode(" " + value));
+    ul.appendChild(li);
+})
+}
