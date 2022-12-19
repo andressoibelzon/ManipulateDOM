d78a13d748efc295cf0a5bb65f1b538bbc2603d0
Andres Soibelzon <a.soibelzon@gmail.com>
2022-12-19 21:57:40 +0000

    changing code

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+    window.onload = function(){
+    
+    let button = document.getElementById("submit");
+    console.log(button);
+    let input = document.getElementById("addToDo");
+    console.log(input);
+
+    button.addEventListener('click', function() {
+        let ul = document.getElementById("list"); 
+        
+        console.log(ul);
+        
+        let paragraph = document.createElement("input");
+        paragraph.innerHTML = input.value;
+        ul.appendchild(paragraph);
+        
+
+    })
+    }
